519109d7c65e38154a847a31ff41a9190501e093
calvin <calvinchaochao@outlook.com>
2022-07-25 22:43:32 +0800

    ../..

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
 // Import rollup plugins
-import html from '@web/rollup-plugin-html';
-import {copy} from '@web/rollup-plugin-copy';
-import resolve from '@rollup/plugin-node-resolve';
-import {terser} from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
-import summary from 'rollup-plugin-summary';
+import html from "@web/rollup-plugin-html";
+import { copy } from "@web/rollup-plugin-copy";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import minifyHTML from "rollup-plugin-minify-html-literals";
+import summary from "rollup-plugin-summary";
 
 export default {
   plugins: [
     // Entry point for application build; can specify a glob to build multiple
     // HTML files for non-SPA app
     html({
-      input: 'src/index.html',
-      format: 'esm',
-        publicPath:"static/"
+      input: "src/index.html",
+      format: "esm",
+      publicPath: "static/",
     }),
     // Resolve bare module specifiers to relative paths
     resolve(),
@@ -30,11 +30,11 @@ export default {
     summary(),
     // Optional: copy any static assets to build directory
     copy({
-      patterns: ['images/**/*'],
+      patterns: ["images/**/*"],
     }),
   ],
   output: {
-    dir: 'build',
+    dir: "build",
   },
-  preserveEntrySignatures: 'strict',
+  preserveEntrySignatures: "strict",
 };
